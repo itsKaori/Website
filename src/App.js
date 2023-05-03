69496721cd5a3fb4506e86087150cec70946efe5
itsKaori <kaorihallsigma@gmail.com>
2023-05-03 16:46:09 -0400

    second commit?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header () {
+  return (
+    <div className="App-header">
+      <h1>about me</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <Header />
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
+
 export default App;
