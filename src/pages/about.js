1635303ad8c536c46c77e395af550fb4bd83354a
itsKaori <kaorihallsigma@gmail.com>
2023-05-08 22:22:04 -0400

    adding source

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Menu from './Menu.js';
+
+function aboutme() {
+  return (
+    <body className="App">
+        <Menu />
+        
+    </body>
+  );
+}
+
+export default App;
